d8a24ee6fa9fff5003729fa841c2ab60d82c9ebd
Mitesh Soni <smiteshhc@gmail.com>
2025-08-14 19:51:05 +0530

    globals: added GenerateAccessToken() and GenerateRefreshToken()

--- internal/globals/globals.go
+++ internal/globals/globals.go
@@ -1,5 +1,52 @@
 package globals
 
-import "github.com/go-chi/jwtauth/v5"
+import (
+	"time"
 
-var TokenAuth *jwtauth.JWTAuth
+	"github.com/go-chi/jwtauth/v5"
+)
+
+var accessTokenAuth *jwtauth.JWTAuth
+var refreshTokenAuth *jwtauth.JWTAuth
+
+func init() {
+	atAlg := "HS256"
+	atSignKey := []byte("a-string-secret-256-bits-long-boom")
+	accessTokenAuth = jwtauth.New(atAlg, atSignKey, nil)
+
+	rtAlg := "HS256"
+	rtSignKey := []byte("b-string-secret-256-bits-long-boom")
+	refreshTokenAuth = jwtauth.New(rtAlg, rtSignKey, nil)
+}
+
+func GenerateAccessToken(userId int) (string, error) {
+	claims := map[string]any{
+		"user_id": userId,
+	}
+
+	jwtauth.SetIssuedNow(claims)
+	jwtauth.SetExpiryIn(claims, 10*time.Minute)
+
+	_, accessToken, err := accessTokenAuth.Encode(claims)
+	if err != nil {
+		return "", err
+	}
+
+	return accessToken, nil
+}
+
+func GenerateRefreshToken(userId int) (string, error) {
+	claims := map[string]any{
+		"user_id": 1,
+	}
+
+	jwtauth.SetIssuedNow(claims)
+	jwtauth.SetExpiryIn(claims, 24*time.Hour)
+
+	_, refreshToken, err := refreshTokenAuth.Encode(claims)
+	if err != nil {
+		return "", err
+	}
+
+	return refreshToken, nil
+}
