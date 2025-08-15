87ed53942bfad63a65a80968cb4476b96b201c3f
Mitesh Soni <smiteshhc@gmail.com>
2025-08-15 18:52:49 +0530

    users: validate user input

--- internal/users/handlers.go
+++ internal/users/handlers.go
@@ -2,10 +2,19 @@ package users
 
 import (
 	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
+	"regexp"
 
 	"github.com/mithcs/probox-api/internal/globals"
+	passValidator "github.com/wagslane/go-password-validator"
 )
+
+type CreateUserRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
 type CreateUserResponse struct {
 	AccessToken  string `json:"accessToken"`
@@ -13,8 +22,25 @@ type CreateUserResponse struct {
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	// get username and password from request body
-	// validate username and password
+	var user CreateUserRequest
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = user.validate()
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 	// store username and hash of password
 	userId := 1
 
@@ -36,4 +62,24 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write(response)
+}
+
+func (user *CreateUserRequest) validate() error {
+	// must start with alphabetic character
+	// may include underscore and/or number
+	// min 3 chars
+	// max 16 chars
+	r := regexp.MustCompile(`^[A-Za-z]\w{2,15}$`)
+	if !r.MatchString(user.Username) {
+		return errors.New("Invalid username.")
+	}
+
+	// if the password has enough entropy, err is nil
+	// otherwise, a formatted error message is provided
+	err := passValidator.Validate(user.Password, 60)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- internal/users/handlers_test.go
+++ internal/users/handlers_test.go
@@ -10,34 +10,97 @@ import (
 )
 
 func TestCreateUser(t *testing.T) {
-	req := httptest.NewRequest(http.MethodPost, "/users", nil)
-	res := httptest.NewRecorder()
-	CreateUser(res, req)
+	t.Run("invalid username", func(t *testing.T) {
+		userReq := CreateUserRequest{
+			Username: "",
+			Password: "correct horse battery staple",
+		}
+		data, err := json.Marshal(userReq)
+		if err != nil {
+			t.Errorf("got err %v, expected nil", err)
+		}
+		body := strings.NewReader(string(data))
 
-	gotCode := res.Code
-	wantCode := http.StatusOK
+		req := httptest.NewRequest(http.MethodPost, "/users", body)
+		res := httptest.NewRecorder()
+		CreateUser(res, req)
 
-	if gotCode != wantCode {
-		t.Errorf("got status code %d, expected %d", gotCode, wantCode)
-	}
+		gotCode := res.Code
+		wantCode := http.StatusBadRequest
 
-	gotBody, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Errorf("got err %v, expected nil", err)
-	}
+		if gotCode != wantCode {
+			t.Errorf("got status code %d, expected %d", gotCode, wantCode)
+		}
+		// test body here
+	})
 
-	var userRes CreateUserResponse
-	err = json.Unmarshal(gotBody, &userRes)
-	if err != nil {
-		t.Errorf("got err %v, expected nil", err)
-	}
+	t.Run("invalid password", func(t *testing.T) {
+		userReq := CreateUserRequest{
+			Username: "example",
+			Password: "",
+		}
+		data, err := json.Marshal(userReq)
+		if err != nil {
+			t.Errorf("got err %v, expected nil", err)
+		}
+		body := strings.NewReader(string(data))
 
-	// checks only `{ "alg":` part to verify whether its JWT
-	if !strings.HasPrefix(userRes.AccessToken, "eyJhbGciOi") {
-		t.Error("invalid access token")
-	}
+		req := httptest.NewRequest(http.MethodPost, "/users", body)
+		res := httptest.NewRecorder()
+		CreateUser(res, req)
 
-	if !strings.HasPrefix(userRes.RefreshToken, "eyJhbGciOi") {
-		t.Error("invalid refresh token")
+		gotCode := res.Code
+		wantCode := http.StatusBadRequest
+
+		if gotCode != wantCode {
+			t.Errorf("got status code %d, expected %d", gotCode, wantCode)
+		}
+		// test body here
+	})
+
+	t.Run("valid username and password", func(t *testing.T) {
+		userReq := CreateUserRequest{
+			Username: "example",
+			Password: "correct horse battery staple",
+		}
+		data, err := json.Marshal(userReq)
+		if err != nil {
+			t.Errorf("got err %v, expected nil", err)
+		}
+		body := strings.NewReader(string(data))
+
+		req := httptest.NewRequest(http.MethodPost, "/users", body)
+		res := httptest.NewRecorder()
+		CreateUser(res, req)
+
+		gotCode := res.Code
+		wantCode := http.StatusOK
+
+		if gotCode != wantCode {
+			t.Errorf("got status code %d, expected %d", gotCode, wantCode)
+		}
+
+		gotBody, err := io.ReadAll(res.Body)
+		if err != nil {
+			t.Errorf("got err %v, expected nil", err)
+		}
+
+		var userRes CreateUserResponse
+		err = json.Unmarshal(gotBody, &userRes)
+		if err != nil {
+			t.Errorf("got err %v, expected nil", err)
+		}
+
+		assertJWT(t, userRes.AccessToken, "access token")
+		assertJWT(t, userRes.RefreshToken, "refresh token")
+	})
+}
+
+func assertJWT(t *testing.T, jwt string, tokenType string) {
+	t.Helper()
+
+	if !strings.HasPrefix(jwt, "eyJhbGciOi") ||
+		strings.Count(jwt, ".") != 2 {
+		t.Errorf("invalid %s", tokenType)
 	}
 }
