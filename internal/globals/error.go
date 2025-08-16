bdabf6ce8388ea974a4437c4e474387e7d2c436d
Mitesh Soni <smiteshhc@gmail.com>
2025-08-16 16:52:31 +0530

    globals: add ReturnErrorResponse()

--- internal/globals/error.go
+++ internal/globals/error.go
@@ -1,6 +1,19 @@
 package globals
 
+import "encoding/json"
+
 type ErrorResponse struct {
-	Title   string
-	Details string
+	Title   string `json:"title"`
+	Details string `json:"details"`
+}
+
+func ReturnErrorResponse(title string, details string) []byte {
+	error := ErrorResponse{
+		Title:   title,
+		Details: details,
+	}
+
+	res, _ := json.Marshal(error)
+
+	return res
 }
