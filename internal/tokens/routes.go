ddc9400d560d06373bc8768b30617166a7109046
Mitesh Soni <smiteshhc@gmail.com>
2025-08-16 17:54:09 +0530

    tokens: protect PUT /tokens route

--- internal/tokens/routes.go
+++ internal/tokens/routes.go
@@ -1,8 +1,16 @@
 package tokens
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth/v5"
+	"github.com/mithcs/probox-api/internal/globals"
+)
 
 func SetupTokens(router *chi.Mux) {
 	router.Post("/tokens", CreateTokens)
-	router.Put("/tokens", RefreshTokens)
+
+	router.
+		With(jwtauth.Verifier(globals.RefreshTokenAuth)).
+		With(globals.AuthenticatorMiddleware(globals.RefreshTokenAuth)).
+		Put("/tokens", RefreshTokens)
 }
