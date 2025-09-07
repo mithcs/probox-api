7bc27433cb67e54a78e43b7583e81062c1f0ef31
Mitesh Soni <smiteshhc@gmail.com>
2025-09-07 20:14:19 +0530

    globals: add EncodeJson() and update ParseBody()

--- internal/globals/misc.go
+++ internal/globals/misc.go
@@ -3,16 +3,42 @@ package globals
 import (
 	"encoding/json"
 	"io"
+	"net/http"
 )
 
-func ParseBody[T any](body io.ReadCloser) (T, error) {
+func ParseBody[T any](body io.ReadCloser) (T, *HTTPError) {
 	var v T
 
 	readBody, err := io.ReadAll(body)
 	if err != nil {
-		return v, err
+		return v, &HTTPError{
+			Status:      http.StatusInternalServerError,
+			Title:       "Read Error.",
+			Description: "Could not read body.",
+		}
 	}
 
 	err = json.Unmarshal(readBody, &v)
-	return v, err
+	if err != nil {
+		return v, &HTTPError{
+			Status:      http.StatusInternalServerError,
+			Title:       "Decode Error.",
+			Description: "Could not unmarshal json.",
+		}
+	}
+
+	return v, nil
+}
+
+func GetJson(v any) ([]byte, *HTTPError) {
+	json, err := json.Marshal(v)
+	if err != nil {
+		return []byte(""), &HTTPError{
+			Status:      http.StatusInternalServerError,
+			Title:       "Encode Error.",
+			Description: "Could not marshal json.",
+		}
+	}
+
+	return json, nil
 }
