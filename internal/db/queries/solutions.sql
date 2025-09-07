65745e1f68661d9ebe090c0e4dc7d01130292a89
Mitesh Soni <smiteshhc@gmail.com>
2025-09-07 12:52:51 +0530

    db: add owner_id and owner_name in solutions
    - also update sqlc to 1.30

--- internal/db/queries/solutions.sql
+++ internal/db/queries/solutions.sql
@@ -1,18 +1,18 @@
 -- name: CreateSolution :one
 INSERT INTO solutions (
-    problem_id, title, description
+    problem_id, title, description, owner_id, owner_name
 ) VALUES (
-    $1, $2, $3
+    $1, $2, $3, $4, $5
 )
 RETURNING *;
 
 -- name: GetSolutions :many
-SELECT id, problem_id, title, description FROM solutions;
+SELECT id, problem_id, title, description, owner_id, owner_name FROM solutions;
 
 -- name: GetSolutionsByProblemID :many
-SELECT id, problem_id, title, description FROM solutions
+SELECT id, problem_id, title, description, owner_id, owner_name FROM solutions
 WHERE problem_id = $1;
 
 -- name: GetSolutionByID :one
-SELECT id, problem_id, title, description FROM solutions
+SELECT id, problem_id, title, description, owner_id, owner_name FROM solutions
 WHERE id = $1 LIMIT 1;
--- internal/db/schemas/solutions.sql
+++ internal/db/schemas/solutions.sql
@@ -3,6 +3,9 @@ CREATE TABLE solutions (
     problem_id   BIGINT       NOT NULL CHECK (problem_id > 0),
     title        VARCHAR(120) NOT NULL,
     description  TEXT         NOT NULL,
+    owner_id     BIGINT       NOT NULL,
+    owner_name   VARCHAR(24)  NOT NULL,
 
-    FOREIGN KEY (problem_id) REFERENCES problems(id)
+    FOREIGN KEY (problem_id) REFERENCES problems(id),
+    FOREIGN KEY (owner_id, owner_name) REFERENCES users(id, full_name)
 );
